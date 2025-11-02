b592c4f01a84380bcc1fb37c3ca2dc84df5a7efb
Justhom <music.justhom@gmail.com>
2025-11-02 12:01:49 +0100

    feat: change le texte affiché

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, Peer-to-Peer world! I learning GitHub mechanism!" << endl;
+    cout << "Hello, Peer-to-Peer world! I am learning GitHub mechanism!" << endl;
     return 0;
 }
