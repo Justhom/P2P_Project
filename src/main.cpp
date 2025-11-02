11bcc1fb10b75a3148c5c6d7813974f90a137b43
Justhom <music.justhom@gmail.com>
2025-11-02 14:50:53 +0100

    changement de print

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, Peer-to-Peer world! I am learning GitHub mechanism!" << endl;
+    cout << "Hello, Peer-to-Peer world! I am learning GitHub mechanism! I removed dSYM tracking" << endl;
     return 0;
 }
